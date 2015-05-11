a3ecf6fc7a4d8695642f3e7e12645cbc8b1b40c1
HrvojeBumber <hrvoje.bumber@hotmail.com>
2015-05-11 17:34:39 -0400

    first commit

---
+++ lab2-enterprise/default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab2_enterprise
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //copy value from textbox to the label
+            lblFirstname.Text = txtFirstname.Text;
+     
+
+            //copy value from textbox to the label
+            lblAddress.Text = txtAddress.Text;
+
+            //
+            lblPassword.Text = txtPassword.Text;
+
+            //show the selected province in the literal control
+            lblFirstname.Text = ddlProvince.SelectedValue + " " + ddlProvince.SelectedItem.Text;
+
+           
+            
+            //loop through the skills and print out the selected skills list
+            foreach (ListItem item in cblSkilllist.Items)
+            {
+                if (item.Selected)
+                {
+                    ltlSkilllist.Text += item.Text + " ";
+                }
+            }
+        }
+    }
+}
